a00aec3d497e5a48745e4e2ca23a48b3d209c756
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-11-30 13:12:18 -0500

    API now returns JSONAPI response for /user

--- src/app/api/user.service.js
+++ src/app/api/user.service.js
@@ -4,8 +4,7 @@
 	module.factory( 'User', function ( $log, $resource, Settings ) {
 		var returnDataAttr = function ( response ) {
 			response.data.permissions.countries = angular.copy(response.data.permissions.country);
-			//return response.data.data;
-			return response.data;
+			return response.data.data;
 		};
 
 		return $resource( Settings.api.mpdDashboard( '/user' ), {}, {
